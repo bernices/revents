519152fcb097335fba426a7872714c888baa46c5
joey2189 <joeyshek2189@live.hk>
2021-03-03 00:30:03 +0800

    End of section 4

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Container } from 'semantic-ui-react';
+import EventDashboard from '../../features/events/eventDashboard/EventDashboard';
+import NavBar from '../../features/events/eventDashboard/nav/NavBar';
 
 function App() {
+
+  const [formOpen,setFormOpen] = useState(false);
+  const [selectedEvent,setSelectedEvent] = useState(null);
+
+  function handleSelectEvent(event){
+    setSelectedEvent(event);
+    setFormOpen(true);
+}
+
+  function handleCreateFormOpen(event){
+    setSelectedEvent(null);
+    setFormOpen(true);
+  }
+
   return (
     <div className="App">
-      <h1>Re-vents</h1>
+      <h1>Revents</h1>
+      <NavBar setFormOpen={handleCreateFormOpen}/>
+      <Container className='main'>
+      <EventDashboard formOpen={formOpen}
+       setFormOpen={setFormOpen}
+       selectEvent={handleSelectEvent}
+       selectedEvent={selectedEvent}
+       />
+      </Container>
     </div>
   );
 }
